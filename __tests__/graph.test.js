33a3376340aac78239e04ea93a06b624ac89dfd4
Taylor Phillips <taylorphillips133@gmail.com>
2020-12-08 13:50:45 -0800

    building a node set with edges.

---
+++ __tests__/graph.test.js
@@ -0,0 +1,42 @@
+import Graph from '../src/graph.js';
+
+describe('Graph', () => {
+
+  let graph = new Graph();
+
+  afterEach(() => {
+    graph = new Graph();
+  });
+
+  test('should correctly instantiate a graph', () => {
+    expect(graph.adjacencyList.size).toEqual(0);
+  });
+
+  test('should add a new node', () => {
+    graph.addNode("Jasmine");
+    expect(graph.adjacencyList.get("Jasmine").size).toEqual(0);
+  });
+
+  test('should return null if the node does not exist in the adjacency list', () => {
+    expect(graph.hasNode("Ada")).toEqual(false);
+  });
+
+  test('should return true if the node does exists in the graph', () => {
+    graph.addNode("Jasmine");
+    expect(graph.hasNode("Jasmine")).toEqual(true);
+  });
+
+  test('add an edge between two nodes', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    graph.createEdge("Jasmine", "Ada");
+    expect(graph.adjacencyList.get("Jasmine").has("Ada")).toEqual(true);
+    expect(graph.adjacencyList.get("Ada").has("Jasmine")).toEqual(true);
+  });
+
+  test('check to see if edge exists in graph', () => {
+    graph.addNode("Jasmine");
+    graph.addNode("Ada");
+    expect(graph.hasEdge("Jasmine", "Ada")).toEqual(false);
+  });
+});
---
+++ src/graph.js
@@ -0,0 +1,24 @@
+export default class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+  addNode(name) {
+    this.adjacencyList.set(name, new Set());
+  }
+  hasNode(name) {
+    if (this.adjacencyList.get(name)) {
+      return true;
+      }
+    return false;
+  }
+  createEdge(node1, node2) {
+    let set1 = this.adjacencyList.get(node1);
+    let set2 = this.adjacencyList.get(node2);
+    set1.add(node2);
+    set2.add(node1);
+  }
+  // createEdge(node1, node2) {  //could also be more concise like this
+  //   this.adjacencyList.get(node1).add(node2);
+  //   this.adjacencyList.get(node2).add(node1);
+  // }
+}
